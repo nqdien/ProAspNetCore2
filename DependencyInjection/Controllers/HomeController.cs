55b3cafe4aec7cd89601004c75b6ce73a05cd66b
Dien Nguyen <diennq@mti-tech.vn>
2017-11-15 14:28:14 +0700

    Add bower

--- DependencyInjection/Controllers/HomeController.cs
+++ DependencyInjection/Controllers/HomeController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DependencyInjection.Models;
 
 namespace DependencyInjection.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public ViewResult Index() => View(new MemoryRepository().Products);
     }
 }
