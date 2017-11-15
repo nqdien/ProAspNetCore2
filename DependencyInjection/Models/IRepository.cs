f65f660a6e2e40f82dca948fa967977357f974ea
Dien Nguyen <diennq@mti-tech.vn>
2017-11-15 12:56:41 +0700

    Dependency Injection

---
+++ DependencyInjection/Models/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        Product this
+    }
+}
