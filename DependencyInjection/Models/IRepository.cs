ff4cd9e95aae9ecbb8aee4900975cafec73f5cd7
Dien Nguyen <diennq@mti-tech.vn>
2017-11-15 13:43:35 +0700

    Add .gitignore

--- DependencyInjection/Models/IRepository.cs
+++ DependencyInjection/Models/IRepository.cs
@@ -8,6 +8,8 @@ namespace DependencyInjection.Models
     public interface IRepository
     {
         IEnumerable<Product> Products { get; }
-        Product this
+        Product this[string name] { get; }
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
     }
 }
